84985a7ac91c96002afadd24d6a986b4b559f951
frederic <frederic.barraquand@u-bordeaux.fr>
2018-06-15 18:31:59 +0200

    lf

--- 10species/data/10Species_dataGeneration.R
+++
@@ -1,8 +0,0 @@
-####################################################################################
-### FB 12/06/2018 -- Generating replicated datasets for 10 species #################
-### Both Lotka-Volterra discrete-time (Ricker) competition and VAR(1) (Gompertz) ###
-####################################################################################
-
-### Mainly competition dataset
-
-
