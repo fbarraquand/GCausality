71cd9d78e72f405dcd6709abf2755e23b99005c0
frederic <frederic.barraquand@u-bordeaux.fr>
2018-06-18 19:00:05 +0200

    Added new code with surrogates for CCM testing

--- twoSpecies_andDriver/2competitors_and_climate_withSurrogates.R
+++ twoSpecies_andDriver/2competitors_and_climate_withSurrogates.R
@@ -3,15 +3,21 @@
 
 ### Modified 18/06/2017 -- added code to test for significance using surrogates
 ### Install new rEDM package, last development version
-library(devtools)
-devtools::install_github("ha0ye/rEDM")
+# library(devtools)
+# devtools::install_github("ha0ye/rEDM")
+
+rm(list=ls())
+dev.off()
+
+## Loading packages
+library(rEDM)
 library(vars)
 
 ### First case with interactions
-
 set.seed(42)
 
 tmax=300
+sigma = 0.3 #process noise = sigma^2 = 0.1
 Y=matrix(1,nrow=tmax,ncol=2)
 Y[1,1]=abs(rnorm(1,1,1))
 Y[1,2]=abs(rnorm(1,1,1))
@@ -23,8 +29,8 @@ y1<-seasonality+y1noise
 ##y2<-seasonality+y2noise
 
 for (t in 1:(tmax-1)){
-  Y[t+1,1] = Y[t,1]*exp(3+0.5*y1[t] - 4*Y[t,1]-2*Y[t,2] + rnorm(1,0,0.1))
-  Y[t+1,2] = Y[t,2]*exp(2.1+0.5*y1[t] -0.31*Y[t,1]-3.1*Y[t,2] + rnorm(1,0,0.1))
+  Y[t+1,1] = Y[t,1]*exp(3+0.5*y1[t] - 4*Y[t,1]-2*Y[t,2] + rnorm(1,0,sigma))
+  Y[t+1,2] = Y[t,2]*exp(2.1+0.5*y1[t] -0.31*Y[t,1]-3.1*Y[t,2] + rnorm(1,0,sigma))
 }
 y=log(Y)
 
@@ -48,8 +54,8 @@ Y[1,2]=abs(rnorm(1,1,1))
 
 
 for (t in 1:(tmax-1)){
-  Y[t+1,1] = Y[t,1]*exp(3+0.5*y1[t] - 4*Y[t,1]-0*Y[t,2] + rnorm(1,0,0.1))
-  Y[t+1,2] = Y[t,2]*exp(2.1+0.5*y1[t] -0*Y[t,1]-3.1*Y[t,2] + rnorm(1,0,0.1))
+  Y[t+1,1] = Y[t,1]*exp(3+0.5*y1[t] - 4*Y[t,1]-0*Y[t,2] + rnorm(1,0,sigma))
+  Y[t+1,2] = Y[t,2]*exp(2.1+0.5*y1[t] -0*Y[t,1]-3.1*Y[t,2] + rnorm(1,0,sigma))
 }
 y=log(Y)
 
@@ -66,34 +72,6 @@ causality(varcompet2bis,cause="y1") #p-value 1.073e-06
 causality(varcompet2bis,cause="y2") #0.1953
 
 
-### Let's use another simulation with just a little more noise
-
-# Y=matrix(1,nrow=tmax,ncol=2)
-# Y[1,1]=abs(rnorm(1,1,1))
-# Y[1,2]=abs(rnorm(1,1,1))
-# 
-# for (t in 1:(tmax-1)){
-#   Y[t+1,1] = Y[t,1]*exp(3+0.5*y1[t] - 4*Y[t,1]-0*Y[t,2] + rnorm(1,0,0.3))
-#   Y[t+1,2] = Y[t,2]*exp(2.1+0.5*y1[t] -0*Y[t,1]-3.1*Y[t,2] + rnorm(1,0,0.3))
-# }
-# y=log(Y)
-# 
-# varcompet2ter<-VAR(y, type="none",exogen=y1,lag.max=10,ic="SC")
-# summary(varcompet2ter)
-# causality(varcompet2ter,cause="y1") #p-value 6.263e-10
-# causality(varcompet2ter,cause="y2") #0.5799
-
-### So it is possible to craft examples that are difficult to infer with GC 
-### --- still we may need many simulations and parameter values for a good comparison of CCM and GC
-
-### NB might well be possible that we would get better results by restricting the fit to a VAR1
-### But we have no theoretical reason to do that
-
-############## Two things to check though!!! ###################
-### I haven't tested pairwise GC on these examples. Perhaps it works better than conditional GC? 
-### And I haven't CCM this, on the other hand. 
-### They say you don't need necessarily corrections for seasonality in the 2012 paper -- or do we? cf Deyle PNAS Influenza
-
 ############# Pairwise GC (without conditioning on the driver) & CCM to test absolutely first... 
 
 varcompet_noExo=VAR(y, type="none",lag.max=10,ic="SC") ## Direct pairwise GC
@@ -101,66 +79,191 @@ summary(varcompet_noExo)
 causality(varcompet_noExo,cause="y1") #p-value 0.03633
 causality(varcompet_noExo,cause="y2") #0.00000
 
-### OK we do have a problem -- but what does CCM say? 
+### We find interactions that are not present
+ 
+### check with grangertest() -- Wald test for nested models
+grangertest(y[,1],y[,2])
+grangertest(y[,2],y[,1]) ### there we find a causality. 
 
 ########## CCM analysis of this dataset ######################
 
-
 ### Merging to a dataframe
 
-species2_species1_temp=data.frame(1:tmax,y,y1)
-names(species2_species1_temp)=c("time","species1","species2","temp")
+S12T=data.frame(1:tmax,y,y1) #species2_species1_temp
+names(S12T)=c("time","species1","species2","temp")
+par(mfrow=c(3,1))
+plot(S12T$time,S12T$species1,type='o')
+plot(S12T$time,S12T$species2,type='o')
+plot(S12T$time,S12T$temp,type='o')
+spectrum(S12T$temp,method="ar") #1/24 = frequency
 
-pdf(file="CCM_2competitors_and_envDriver.pdf",height=10,width=10)
-par(mfrow=c(2,2),cex=1.25,lwd=2)
-species1_xmap_temp <- ccm(species2_species1_temp, E = 3, lib_column = "species1", 
-                      target_column = "temp", lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+### Old analysis with previous package below, commented out
+### New analysis
+### Adapted from https://cran.r-project.org/web/packages/rEDM/vignettes/rEDM-tutorial.html
 
-temp_xmap_species1 <- ccm(species2_species1_temp, E = 3, lib_column = "temp", target_column = "species1", 
-                      lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+### Basic CCM analysis
+varnames <- colnames(S12T[2:4])
+n <- NROW(S12T)
+ccm_matrix <- array(NA, dim = c(length(varnames), length(varnames)), dimnames = list(varnames, 
+                                                                             varnames))
 
-a_xmap_t_means <- ccm_means(species1_xmap_temp)
-t_xmap_a_means <- ccm_means(temp_xmap_species1)
+for (ccm_from in varnames) {
+  for (ccm_to in varnames[varnames != ccm_from]) {
+    out_temp <- ccm(S12T, E = 8, lib_column = ccm_from, target_column = ccm_to, 
+                    lib_sizes = n, replace = FALSE, silent = TRUE)
+    ccm_matrix[ccm_from, ccm_to] <- out_temp$rho
+  }
+}
 
-par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
-plot(a_xmap_t_means$lib_size, pmax(0, a_xmap_t_means$rho), type = "l", col = "red", 
-     xlab = "Library Size", ylab = "Cross Map Skill (rho)", ylim = c(0, 1.1))
-lines(t_xmap_a_means$lib_size, pmax(0, t_xmap_a_means$rho), col = "blue")
-legend(x = "topleft", legend = c("species1 xmap temp", "temp xmap species1"), col = c("red", "blue"), lwd = 1, inset = 0.02, cex = 0.8)
+### ------------- Generate surrogate time series ----------------------####
+num_surr <- 1000 #number of surrogate time series 
+surr_species1 <- make_surrogate_data(S12T$species1, method = "seasonal", 
+                                 T_period = 24, num_surr = num_surr)
+surr_species2 <- make_surrogate_data(S12T$species2, method = "seasonal", 
+                                     T_period = 24, num_surr = num_surr)
+surr_temp <- make_surrogate_data(S12T$temp, method = "seasonal", 
+                                 T_period = 12, num_surr = num_surr)
 
-### Checking between species2 and species1
+### ------------ Compute the rho values for the surrogates -------------####
+### Regarding the effect of temp and species 2 on species 1 
+rho_surr1 <- data.frame(temp = numeric(num_surr), species2 = numeric(num_surr))
+### Regarding the effect of temp and species 1 on species 2
+rho_surr2 <- data.frame(temp = numeric(num_surr), species1 = numeric(num_surr))
 
-species1_xmap_species2 <- ccm(species2_species1_temp, E = 3, lib_column = "species1", 
-                      target_column = "species2", lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+for (i in 1:num_surr) {
+  rho_surr1$temp[i] <- ccm(cbind(S12T$species1, surr_temp[,i]), E = 3, lib_column = 1, target_column = 2, lib_sizes = NROW(S12T), 
+                           replace = FALSE)$rho
+  
+  rho_surr1$species2[i] <- ccm(cbind(S12T$species1, surr_species2[,i]), E = 3, lib_column = 1, target_column = 2, lib_sizes = NROW(S12T), 
+                          replace = FALSE)$rho
+}
 
-species2_xmap_species1 <- ccm(species2_species1_temp, E = 3, lib_column = "species2", target_column = "species1", 
-                      lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+### P-value for rejecting the hypothesis that seasonality both causes temp and species 1
+(sum(ccm_matrix['species1','temp'] < rho_surr1$temp) + 1) / 
+(length(rho_surr1$temp) + 1) ### we conclude that temp -> species 1
 
-species1_xmap_species2_means <- ccm_means(species1_xmap_species2)
-species2_xmap_species1_means <- ccm_means(species2_xmap_species1)
+### P-value for rejecting the hypothesis that seasonality both causes species 1 and species 2
+# (more interesting here)
+(sum(ccm_matrix['species1','species2'] < rho_surr1$species2) + 1) / 
+  (length(rho_surr1$species2) + 1) ## not directly significant but still low. 
 
-#par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
-plot(species1_xmap_species2_means$lib_size, pmax(0, species1_xmap_species2_means$rho), type = "l", col = "red", xlab = "Library Size", ylab = "Cross Map Skill (rho)",ylim = c(0, 1.1))
-lines(species2_xmap_species1_means$lib_size, pmax(0, species2_xmap_species1_means$rho), col = "blue")
-legend(x = "topleft", legend = c("species1 xmap species2", "species2 xmap species1"), col = c("red", "blue"), lwd = 1, inset = 0.02, cex = 0.8)
+### Perhaps we can have a more refined surrogate? to condition directly on temp?
+?make_surrogate_data
+signal = rnorm(100) + sin(1:100 * pi / 6)
+twin_surr = make_surrogate_twin(signal, 10)
+par(mfrow=c(2,1))
+plot(signal,type='o',pch=19)
+matplot(twin_surr)
+matlines(twin_surr)
 
-# Checking between species2 and temp
+### That's what we want -- let's redo the analysis for 1 -> 2 
+surr_temp_twin <- make_surrogate_data(S12T$temp, method = "twin", num_surr = num_surr)
+surr_species2_twin <- make_surrogate_data(S12T$species2, method = "twin", num_surr = num_surr)
+rho_surr1_twin <- data.frame(temp = numeric(num_surr), species2 = numeric(num_surr))
 
-species2_xmap_temp <- ccm(species2_species1_temp, E = 3, lib_column = "species2", 
-                      target_column = "temp", lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+for (i in 1:num_surr) {
+  rho_surr1_twin$temp[i] <- ccm(cbind(S12T$species1, surr_temp_twin[,i]), E = 3, lib_column = 1, target_column = 2, lib_sizes = NROW(S12T), 
+                           replace = FALSE)$rho
+  
+  rho_surr1_twin$species2[i] <- ccm(cbind(S12T$species1, surr_species2_twin[,i]), E = 3, lib_column = 1, target_column = 2, lib_sizes = NROW(S12T), 
+                               replace = FALSE)$rho
+}
 
-temp_xmap_species2 <- ccm(species2_species1_temp, E = 3, lib_column = "temp", target_column = "species2", 
-                      lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+### P-value for rejecting the hypothesis that seasonality both causes temp and species 1
+(sum(ccm_matrix['species1','temp'] < rho_surr1_twin$temp) + 1) / 
+  (length(rho_surr1$temp) + 1) ### 0.8731269 
+### do we conclude from this that temp does not cause species 1 more than any other variable with similar trends? sounds dubious there
 
-a_xmap_t_means <- ccm_means(species2_xmap_temp)
-t_xmap_a_means <- ccm_means(temp_xmap_species2)
+### P-value for rejecting the hypothesis that seasonality both causes species 1 and species 2
+# (more interesting here)
+(sum(ccm_matrix['species1','species2'] < rho_surr1_twin$species2) + 1) / 
+  (length(rho_surr1$species2) + 1) ## 0.25 not significant -- this works to some degree it seems. 
+### NB we have to check the other way around, i.e., if there is an interaction the technique can detect it. 
+### cf. Cobey and Baskerville results. 
 
-par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
-plot(a_xmap_t_means$lib_size, pmax(0, a_xmap_t_means$rho), type = "l", col = "red", 
-     xlab = "Library Size", ylab = "Cross Map Skill (rho)", ylim = c(0, 1.1))
-lines(t_xmap_a_means$lib_size, pmax(0, t_xmap_a_means$rho), col = "blue")
-legend(x = "topleft", legend = c("species2 xmap temp", "temp xmap species2"), col = c("red", "blue"), lwd = 1, inset = 0.02, cex = 0.8)
-dev.off()
+### Also, the grangertest() only found a (false) interaction 2->1
+# we need to check that direction as well. 
+## we redo the analysis for 2->1
+surr_species1_twin <- make_surrogate_data(S12T$species1, method = "twin", num_surr = num_surr)
+rho_surr2_twin <- data.frame(temp = numeric(num_surr), species1 = numeric(num_surr))
+
+for (i in 1:num_surr) {
+  rho_surr2_twin$temp[i] <- ccm(cbind(S12T$species2, surr_temp_twin[,i]), E = 3, lib_column = 1, target_column = 2, lib_sizes = NROW(S12T), 
+                                replace = FALSE)$rho
+  
+  rho_surr2_twin$species2[i] <- ccm(cbind(S12T$species2, surr_species1_twin[,i]), E = 3, lib_column = 1, target_column = 2, lib_sizes = NROW(S12T), 
+                                    replace = FALSE)$rho
+}
+
+### P-value for rejecting the hypothesis that temp does not cause species 2 fluctuations
+(sum(ccm_matrix['species2','temp'] < rho_surr2_twin$temp) + 1) / 
+  (length(rho_surr2$temp) + 1) ### 0.001998002
+### we reject
+
+### P-value for rejecting the hypothesis that species 1 does not cause species 1 fluctuations
+(sum(ccm_matrix['species2','species1'] < rho_surr2_twin$species2) + 1) / 
+  (length(rho_surr1$species2) + 1) ## almost significant here 
+
+### ------------------- Stopped here 18/06/2018 ----------------------###
+### HAVE I ALL THE LINKS IN THE RIGHT DIRECTIONS? CHECK THAT TOMORROW ###
+######## Some more ideas ################################################
+### What if we used surrogate techniques in a time-domain Granger context? 
+### As we did for Spectral GC!! Would that produce a fair comparison? 
+
+### NB Code with many simulations below is unchanged. Need to do that. 
+### ------------------------------------------------------------------###
+
+#### ------------- Old analysis 08/10/2017 ------------------------------------------####
+# pdf(file="CCM_2competitors_and_envDriver.pdf",height=10,width=10)
+# par(mfrow=c(2,2),cex=1.25,lwd=2)
+# species1_xmap_temp <- ccm(species2_species1_temp, E = 3, lib_column = "species1", 
+#                       target_column = "temp", lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+# 
+# temp_xmap_species1 <- ccm(species2_species1_temp, E = 3, lib_column = "temp", target_column = "species1", 
+#                       lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+# 
+# a_xmap_t_means <- ccm_means(species1_xmap_temp)
+# t_xmap_a_means <- ccm_means(temp_xmap_species1)
+# 
+# par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
+# plot(a_xmap_t_means$lib_size, pmax(0, a_xmap_t_means$rho), type = "l", col = "red", 
+#      xlab = "Library Size", ylab = "Cross Map Skill (rho)", ylim = c(0, 1.1))
+# lines(t_xmap_a_means$lib_size, pmax(0, t_xmap_a_means$rho), col = "blue")
+# legend(x = "topleft", legend = c("species1 xmap temp", "temp xmap species1"), col = c("red", "blue"), lwd = 1, inset = 0.02, cex = 0.8)
+# 
+# ### Checking between species2 and species1
+# 
+# species1_xmap_species2 <- ccm(species2_species1_temp, E = 3, lib_column = "species1", 
+#                       target_column = "species2", lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+# 
+# species2_xmap_species1 <- ccm(species2_species1_temp, E = 3, lib_column = "species2", target_column = "species1", 
+#                       lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+# 
+# species1_xmap_species2_means <- ccm_means(species1_xmap_species2)
+# species2_xmap_species1_means <- ccm_means(species2_xmap_species1)
+# 
+# #par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
+# plot(species1_xmap_species2_means$lib_size, pmax(0, species1_xmap_species2_means$rho), type = "l", col = "red", xlab = "Library Size", ylab = "Cross Map Skill (rho)",ylim = c(0, 1.1))
+# lines(species2_xmap_species1_means$lib_size, pmax(0, species2_xmap_species1_means$rho), col = "blue")
+# legend(x = "topleft", legend = c("species1 xmap species2", "species2 xmap species1"), col = c("red", "blue"), lwd = 1, inset = 0.02, cex = 0.8)
+# 
+# # Checking between species2 and temp
+# 
+# species2_xmap_temp <- ccm(species2_species1_temp, E = 3, lib_column = "species2", 
+#                       target_column = "temp", lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+# 
+# temp_xmap_species2 <- ccm(species2_species1_temp, E = 3, lib_column = "temp", target_column = "species2", 
+#                       lib_sizes = seq(5, 100, by = 5), random_libs = FALSE)
+# 
+# a_xmap_t_means <- ccm_means(species2_xmap_temp)
+# t_xmap_a_means <- ccm_means(temp_xmap_species2)
+# 
+# par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
+# plot(a_xmap_t_means$lib_size, pmax(0, a_xmap_t_means$rho), type = "l", col = "red", 
+#      xlab = "Library Size", ylab = "Cross Map Skill (rho)", ylim = c(0, 1.1))
+# lines(t_xmap_a_means$lib_size, pmax(0, t_xmap_a_means$rho), col = "blue")
+# legend(x = "topleft", legend = c("species2 xmap temp", "temp xmap species2"), col = c("red", "blue"), lwd = 1, inset = 0.02, cex = 0.8)
+# dev.off()
 
 ### We recover the correct causal order for effects of the environment
 ### Though it looks like species 1 affects species 2 with CCM
