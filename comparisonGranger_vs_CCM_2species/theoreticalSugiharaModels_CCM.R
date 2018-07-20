de1ef747e1a9753edb836bf025ac697dfcc55c59
fbarraquand <frederic.barraquand@u-bordeaux.fr>
2018-07-20 17:37:41 +0200

    Refined causal criterion with P-values for CCM

--- comparisonGranger_vs_CCM_2species/theoreticalSugiharaModels_CCM.R
+++ comparisonGranger_vs_CCM_2species/theoreticalSugiharaModels_CCM.R
@@ -4,10 +4,14 @@
 
 #library(devtools)
 #devtools::install_github("ha0ye/rEDM")
-
+rm(list=ls())
 library("vars")
 library("rEDM")
 
+
+### NB This code does not compute the embedding dimension (ED) which is set to 3
+### On the minus side, recomputing the ED could help capture the nonlinearies 
+### On the plus side, we avoid artefacts due to a too large ED when we do 500 simulations. 
 
 ########################################################################################################################
 ########## Sugihara two species deterministic competition model -- interactions
@@ -43,16 +47,7 @@ for (i in 1:2){
 #dev.off()
 
 
-### Do classic CCM analysis in the vein of 
-data(sardine_anchovy_sst)
-anchovy_xmap_sst <- ccm(sardine_anchovy_sst, E = 3, lib_column = "anchovy", 
-                        target_column = "np_sst", lib_sizes = seq(10, 80, by = 10), num_samples = 100, 
-                        random_libs = TRUE, replace = TRUE, silent = TRUE)
-sst_xmap_anchovy <- ccm(sardine_anchovy_sst, E = 3, lib_column = "np_sst", target_column = "anchovy", 
-                        lib_sizes = seq(10, 80, by = 10), num_samples = 100, random_libs = TRUE, 
-                        replace = TRUE, silent = TRUE)
-str(anchovy_xmap_sst)
-
+### Do classic CCM analysis in the vein of sardine / anchovy / sst
 
 species12=data.frame(1:tmax,z)
 names(species12)=c("time","sp1","sp2")
@@ -68,15 +63,15 @@ sp2_xmap_sp1 <- ccm(species12, E = 3, lib_column = "sp2", target_column = "sp1",
 ### Using the same method for producing P-values as Cobey and Baskerville PloS One 2016
 rho1xmap2_Lmin_random <- sp1_xmap_sp2$rho[sp1_xmap_sp2$lib_size ==libsizes[1]]
 rho1xmap2_Lmax_random <- sp1_xmap_sp2$rho[sp1_xmap_sp2$lib_size ==libsizes[lm]]
-# Fraction of samples for which rho(L_max)> rho(L_min)
-Pval_1xmap2 = sum(rho1xmap2_Lmax_random>rho1xmap2_Lmin_random)/numsamples #2 towards 1
-Pval_1xmap2 #0.93
+# Fraction of samples for which rho(L_max)<rho(L_min)
+Pval_1xmap2 = sum(rho1xmap2_Lmax_random<rho1xmap2_Lmin_random)/numsamples #2 towards 1
+Pval_1xmap2 #0.1
 
 rho2xmap1_Lmin_random <- sp2_xmap_sp1$rho[sp2_xmap_sp1$lib_size ==libsizes[1]]
 rho2xmap1_Lmax_random <- sp2_xmap_sp1$rho[sp2_xmap_sp1$lib_size ==libsizes[lm]]
 
-Pval_2xmap1 = sum(rho2xmap1_Lmax_random>rho2xmap1_Lmin_random)/numsamples #1 towards 2
-Pval_2xmap1 #1
+Pval_2xmap1 = sum(rho2xmap1_Lmax_random<rho2xmap1_Lmin_random)/numsamples #1 towards 2
+Pval_2xmap1 #0
 
 ### Let's try to see those
 sp1_xmap_sp2_means <- ccm_means(sp1_xmap_sp2)
@@ -134,17 +129,17 @@ sp2_xmap_sp1 <- ccm(species12, E = 3, lib_column = "sp2", target_column = "sp1",
 ### Using the same method for producing P-values as Cobey and Baskerville PloS One 2016
 rho1xmap2_Lmin_random <- sp1_xmap_sp2$rho[sp1_xmap_sp2$lib_size ==libsizes[1]]
 rho1xmap2_Lmax_random <- sp1_xmap_sp2$rho[sp1_xmap_sp2$lib_size ==libsizes[lm]]
-# Fraction of samples for which rho(L_max)> rho(L_min)
-Pval_1xmap2 = sum(rho1xmap2_Lmax_random>rho1xmap2_Lmin_random)/numsamples #2 towards 1
-Pval_1xmap2 #0.53
+# Fraction of samples for which rho(L_max)<rho(L_min)
+Pval_1xmap2 = sum(rho1xmap2_Lmax_random<rho1xmap2_Lmin_random)/numsamples #2 towards 1
+Pval_1xmap2 #0.49
 
 rho2xmap1_Lmin_random <- sp2_xmap_sp1$rho[sp2_xmap_sp1$lib_size ==libsizes[1]]
 rho2xmap1_Lmax_random <- sp2_xmap_sp1$rho[sp2_xmap_sp1$lib_size ==libsizes[lm]]
 
-Pval_2xmap1 = sum(rho2xmap1_Lmax_random>rho2xmap1_Lmin_random)/numsamples #1 towards 2
-Pval_2xmap1 #0.62
+Pval_2xmap1 = sum(rho2xmap1_Lmax_random<rho2xmap1_Lmin_random)/numsamples #1 towards 2
+Pval_2xmap1 #0.47
 
-### This is not good - let's try and see those
+### Let's try and see those
 sp1_xmap_sp2_means <- ccm_means(sp1_xmap_sp2)
 sp2_xmap_sp1_means <- ccm_means(sp2_xmap_sp1)
 
@@ -153,7 +148,7 @@ plot(sp1_xmap_sp2_means$lib_size, pmax(0, sp1_xmap_sp2_means$rho), type = "l", c
 lines(sp2_xmap_sp1_means$lib_size, pmax(0, sp2_xmap_sp1_means$rho), col = "blue")
 legend(x = "topleft", legend = c("sp1_xmap_sp2", "sp2_xmap_sp1"), col = c("red","blue"), lwd = 1, bty = "n", inset = 0.02, cex = 0.8)
 
-# How do I know there's no increase? Granted, the numbers are low... Should I set a rho_min
+# How do I know there's no increase? Granted, the numbers are low... Should I set a rho_min?
 
 ### Let's try with a different cross-validation setup, 200 for the model and 100 for validation
 
@@ -171,17 +166,17 @@ sp2_xmap_sp1 <- ccm(species12, lib = c(1,200), pred=c(201,300), E = 3, lib_colum
 ### Using the same method for producing P-values as Cobey and Baskerville PloS One 2016
 rho1xmap2_Lmin_random <- sp1_xmap_sp2$rho[sp1_xmap_sp2$lib_size ==libsizes[1]]
 rho1xmap2_Lmax_random <- sp1_xmap_sp2$rho[sp1_xmap_sp2$lib_size ==libsizes[lm]]
-# Fraction of samples for which rho(L_max)> rho(L_min)
-Pval_1xmap2 = sum(rho1xmap2_Lmax_random>rho1xmap2_Lmin_random)/numsamples #2 towards 1
-Pval_1xmap2 #0.63
+# Fraction of samples for which rho(L_max)<rho(L_min)
+Pval_1xmap2 = sum(rho1xmap2_Lmax_random<rho1xmap2_Lmin_random)/numsamples #2 towards 1
+Pval_1xmap2 #0.41
 
 rho2xmap1_Lmin_random <- sp2_xmap_sp1$rho[sp2_xmap_sp1$lib_size ==libsizes[1]]
 rho2xmap1_Lmax_random <- sp2_xmap_sp1$rho[sp2_xmap_sp1$lib_size ==libsizes[lm]]
 
-Pval_2xmap1 = sum(rho2xmap1_Lmax_random>rho2xmap1_Lmin_random)/numsamples #1 towards 2
-Pval_2xmap1 #0.64
+Pval_2xmap1 = sum(rho2xmap1_Lmax_random<rho2xmap1_Lmin_random)/numsamples #1 towards 2
+Pval_2xmap1 #0.46
 
-### This is not good - let's try and see those
+### Let's try and see those
 sp1_xmap_sp2_means <- ccm_means(sp1_xmap_sp2)
 sp2_xmap_sp1_means <- ccm_means(sp2_xmap_sp1)
 
@@ -198,9 +193,13 @@ legend(x = "topleft", legend = c("sp1_xmap_sp2", "sp2_xmap_sp1"), col = c("red",
 ####################### Repeating many times with many initial conditions - yet to do ####################################
 ########################################################################################################################
 
+ncond<-500 #how many initial conditions do we consider? 
+
 #Initializing vectors 
-ncond<-500
-Pval_12_inter=Pval_21_inter=Pval_12_noInter=Pval_21_noInter=lag_order_inter=lag_order_noInter=rep(NA,ncond)
+Pval_12_inter=Pval_21_inter=Pval_12_noInter=Pval_21_noInter=rep(NA,ncond)
+RhoLMax_12_inter=RhoLMax_21_inter=RhoLMax_12_noInter=RhoLMax_21_noInter=rep(NA,ncond)
+index_1cause2_inter=index_2cause1_inter=index_1cause2_noInter=index_2cause1_noInter=rep(NA,ncond)
+lag_order_inter=lag_order_noInter=rep(NA,ncond)
 
 #F: NB I need to replace the lag order by the embedding dimension, so I need to really introduce a code for the simplex
 
@@ -210,8 +209,10 @@ Pval_12_inter=Pval_21_inter=Pval_12_noInter=Pval_21_noInter=lag_order_inter=lag_
 
 
 for (kcond in 1:ncond){
-
+kcond
+  ### Data simulation
 tmax=800
+tmin=501
 X=Y=rep(1,tmax)## Problem if I start with 1
 X[1]=runif(1,0.1,0.7)
 Y[1]=runif(1,0.1,0.7)
@@ -222,26 +223,67 @@ for (t in 1:tmax)
 }
 X
 Y
-x=log(X[501:800])
-y=log(Y[501:800])
+x=log(X[tmin:800])
+y=log(Y[tmin:800])
 # centering
 x=x-mean(x)
 y=y-mean(y)
 z=cbind(x,y)
 
-varcompet<-VAR(y=data.frame(cbind(x,y)), type="none",lag.max=20,ic="SC")
-lag_order_inter[kcond] <- varcompet$p
-## VARselect(z) ### Order selected is 7
-## causality(varcompet,cause="x") #p-value < 2.2e-16
-## causality(varcompet,cause="y")
+ ### determination of the lag order
+ # Here we just assume this one to be 3
+ lag_order_inter[kcond] = 3
+ 
+ ### CCM Analysis 
 
-gxy = grangertest(x,y,order = lag_order_inter[kcond]) #x causes y 
-gyx = grangertest(y,x,order = lag_order_inter[kcond]) #y causes x
+species12=data.frame(tmin:tmax,z)
+names(species12)=c("time","sp1","sp2")
+libsizes = seq(10, 80, by = 10)
+lm=length(libsizes)
+numsamples = 100
+sp1_xmap_sp2 <- ccm(species12, E = lag_order_inter[kcond] , lib_column = "sp1", 
+                    target_column = "sp2", lib_sizes = libsizes, random_libs = TRUE,num_samples = numsamples)
+#can we reconstruct 2 from 1, i.e., does 2 CCM-cause 1?
+sp2_xmap_sp1 <- ccm(species12, E = lag_order_inter[kcond] , lib_column = "sp2", target_column = "sp1", 
+                    lib_sizes = libsizes, random_libs = TRUE, num_samples = numsamples) #can we reconstruct 1 from 2, i.e., does 1 CCM-cause 2?
 
-Pval_12_inter[kcond]=gxy$`Pr(>F)`[2]
-Pval_21_inter[kcond]=gyx$`Pr(>F)`[2]
+### Using the same method for producing P-values as Cobey and Baskerville PloS One 2016
+rho1xmap2_Lmin_random <- sp1_xmap_sp2$rho[sp1_xmap_sp2$lib_size ==libsizes[1]]
+rho1xmap2_Lmax_random <- sp1_xmap_sp2$rho[sp1_xmap_sp2$lib_size ==libsizes[lm]]
+# Fraction of samples for which rho(L_max)<rho(L_min)
+Pval_1xmap2 = sum(rho1xmap2_Lmax_random<rho1xmap2_Lmin_random)/numsamples #2 towards 1
+Pval_1xmap2 #0.93
+
+rho2xmap1_Lmin_random <- sp2_xmap_sp1$rho[sp2_xmap_sp1$lib_size ==libsizes[1]]
+rho2xmap1_Lmax_random <- sp2_xmap_sp1$rho[sp2_xmap_sp1$lib_size ==libsizes[lm]]
+
+Pval_2xmap1 = sum(rho2xmap1_Lmax_random<rho2xmap1_Lmin_random)/numsamples #1 towards 2
+Pval_2xmap1 #1
+
+### Let's try to see those
+sp1_xmap_sp2_means <- ccm_means(sp1_xmap_sp2)
+sp2_xmap_sp1_means <- ccm_means(sp2_xmap_sp1)
+
+plot(sp1_xmap_sp2_means$lib_size, pmax(0, sp1_xmap_sp2_means$rho), type = "l", col = "red", 
+     xlab = "Library Size", ylab = "Cross Map Skill (rho)", ylim = c(0, 1))
+lines(sp2_xmap_sp1_means$lib_size, pmax(0, sp2_xmap_sp1_means$rho), col = "blue")
+legend(x = "topleft", legend = c("sp1_xmap_sp2", "sp2_xmap_sp1"), col = c("red","blue"), lwd = 1, bty = "n", inset = 0.02, cex = 0.8)
+
+
+Pval_12_inter[kcond]=Pval_2xmap1 # 1 causes 2 if 2 xmap 1
+Pval_21_inter[kcond]=Pval_1xmap2 # 2 causes 1 if 1 xmap 2
+
+RhoLMax_12_inter[kcond]=sp2_xmap_sp1_means$rho[sp2_xmap_sp1_means$lib_size==libsizes[lm]] # 1 causes 2 if 2 xmap 1
+RhoLMax_21_inter[kcond]=sp1_xmap_sp2_means$rho[sp1_xmap_sp2_means$lib_size==libsizes[lm]] # 2 causes 1 if 1 xmap 2
+
+if ((Pval_12_inter[kcond]<0.1)&(RhoLMax_12_inter[kcond]>0.1))
+{index_1cause2_inter[kcond]=1} else {index_1cause2_inter[kcond]=0}
+
+if ((Pval_21_inter[kcond]<0.1)&(RhoLMax_21_inter[kcond]>0.1))
+{index_2cause1_inter[kcond]=1} else {index_2cause1_inter[kcond]=0}
 
 }
+
 
 ########################################################################################################################
 ########## Sugihara two species deterministic competition model -- no interactions
@@ -249,7 +291,7 @@ Pval_21_inter[kcond]=gyx$`Pr(>F)`[2]
 
 
 for (kcond in 1:ncond){
-  
+  kcond
   tmax=800
   X=Y=rep(1,tmax)## Problem if I start with 1
   X[1]=runif(1,0.1,0.7)
@@ -268,38 +310,87 @@ for (kcond in 1:ncond){
   y=y-mean(y)
   z=cbind(x,y)
   
-  varcompet<-VAR(y=data.frame(cbind(x,y)), type="none",lag.max=20,ic="SC")
-  lag_order_noInter[kcond] <- varcompet$p
+  ### determination of the lag order
+  # Here we just assume this one to be 3
+  lag_order_noInter[kcond] = 3
   
-  gxy = grangertest(x,y,order = lag_order_noInter[kcond]) #x causes y 
-  gyx = grangertest(y,x,order = lag_order_noInter[kcond]) #y causes x
+  ### CCM Analysis 
   
-  Pval_12_noInter[kcond]=gxy$`Pr(>F)`[2]
-  Pval_21_noInter[kcond]=gyx$`Pr(>F)`[2]
+  species12=data.frame(tmin:tmax,z)
+  names(species12)=c("time","sp1","sp2")
+  libsizes = seq(10, 80, by = 10)
+  lm=length(libsizes)
+  numsamples = 100
+  sp1_xmap_sp2 <- ccm(species12, E = lag_order_noInter[kcond] , lib_column = "sp1", 
+                      target_column = "sp2", lib_sizes = libsizes, random_libs = TRUE,num_samples = numsamples)
+  #can we reconstruct 2 from 1, i.e., does 2 CCM-cause 1?
+  sp2_xmap_sp1 <- ccm(species12, E = lag_order_noInter[kcond] , lib_column = "sp2", target_column = "sp1", 
+                      lib_sizes = libsizes, random_libs = TRUE, num_samples = numsamples) #can we reconstruct 1 from 2, i.e., does 1 CCM-cause 2?
+  
+  ### Using the same method for producing P-values as Cobey and Baskerville PloS One 2016
+  rho1xmap2_Lmin_random <- sp1_xmap_sp2$rho[sp1_xmap_sp2$lib_size ==libsizes[1]]
+  rho1xmap2_Lmax_random <- sp1_xmap_sp2$rho[sp1_xmap_sp2$lib_size ==libsizes[lm]]
+  # Fraction of samples for which rho(L_max)<rho(L_min)
+  Pval_1xmap2 = sum(rho1xmap2_Lmax_random<rho1xmap2_Lmin_random)/numsamples #2 towards 1
+  Pval_1xmap2 #0.93
+  
+  rho2xmap1_Lmin_random <- sp2_xmap_sp1$rho[sp2_xmap_sp1$lib_size ==libsizes[1]]
+  rho2xmap1_Lmax_random <- sp2_xmap_sp1$rho[sp2_xmap_sp1$lib_size ==libsizes[lm]]
+  
+  Pval_2xmap1 = sum(rho2xmap1_Lmax_random<rho2xmap1_Lmin_random)/numsamples #1 towards 2
+  Pval_2xmap1 #1
+  
+  ### Let's try to see those
+  sp1_xmap_sp2_means <- ccm_means(sp1_xmap_sp2)
+  sp2_xmap_sp1_means <- ccm_means(sp2_xmap_sp1)
+  
+  plot(sp1_xmap_sp2_means$lib_size, pmax(0, sp1_xmap_sp2_means$rho), type = "l", col = "red", 
+       xlab = "Library Size", ylab = "Cross Map Skill (rho)", ylim = c(0, 1))
+  lines(sp2_xmap_sp1_means$lib_size, pmax(0, sp2_xmap_sp1_means$rho), col = "blue")
+  legend(x = "topleft", legend = c("sp1_xmap_sp2", "sp2_xmap_sp1"), col = c("red","blue"), lwd = 1, bty = "n", inset = 0.02, cex = 0.8)
+  
+  
+  Pval_12_noInter[kcond]=Pval_2xmap1 # 1 causes 2 if 2 xmap 1
+  Pval_21_noInter[kcond]=Pval_1xmap2 # 2 causes 1 if 1 xmap 2
+  
+  RhoLMax_12_noInter[kcond]=sp2_xmap_sp1_means$rho[sp2_xmap_sp1_means$lib_size==libsizes[lm]] # 1 causes 2 if 2 xmap 1
+  RhoLMax_21_noInter[kcond]=sp1_xmap_sp2_means$rho[sp1_xmap_sp2_means$lib_size==libsizes[lm]] # 2 causes 1 if 1 xmap 2
+  
+  if ((Pval_12_noInter[kcond]<0.1)&(RhoLMax_12_noInter[kcond]>0.1))
+  {index_1cause2_noInter[kcond]=1} else {index_1cause2_noInter[kcond]=0}
+  
+  if ((Pval_21_noInter[kcond]<0.1)&(RhoLMax_21_noInter[kcond]>0.1))
+  {index_2cause1_noInter[kcond]=1} else {index_2cause1_noInter[kcond]=0}
   
 }
 
-par(mfrow=c(1,2))
-sm.density.compare(c(Pval_12_inter,Pval_12_noInter), group = c(1,0), model = "none",lwd=2)
-sm.density.compare(c(Pval_21_inter,Pval_21_noInter), group = c(1,0), model = "none",lwd=2)
-### Poor representation
+#Fraction of 1->2 when present
+sum(index_1cause2_inter)/ncond
+#Fraction of 2->1 when present
+sum(index_2cause1_inter)/ncond
 
-DataCompet_sugiharaDeterModel = data.frame(lag_order_inter,Pval_12_inter,Pval_21_inter,lag_order_noInter,Pval_12_noInter,Pval_21_noInter)
+#Fraction of 1->2 when absent
+sum(index_1cause2_noInter)/ncond
+#Fraction of 2->1 when absent
+sum(index_2cause1_noInter)/ncond
 
+#### What would have happened if we only used P-values?
 
-### Let's try the percentage of initial conditions at which the Granger tests works
-sum(Pval_12_inter<0.1)/length(Pval_12_inter) #100% at 0.1 level
-sum(Pval_21_inter<0.1)/length(Pval_21_inter) #52% at 0.1 level
+#Fraction of 1->2 when present
+sum(Pval_12_inter<0.1)/ncond
+#Fraction of 2->1 when present
+sum(Pval_21_inter<0.1)/ncond
 
-sum(Pval_12_noInter>0.1)/length(Pval_12_noInter) #90% at 0.1 level
-sum(Pval_21_noInter>0.1)/length(Pval_21_noInter) #87% at 0.1 level
+#Fraction of 1->2 when absent
+sum(Pval_12_noInter<0.1)/ncond
+#Fraction of 2->1 when absent
+sum(Pval_21_noInter<0.1)/ncond
+Pval_21_noInter
 
-### Plot 4 panel figure with (a) P-values 1->2, (b) P-values 2->1 interactions and no interactions
-### Other plot with lag order - should I overlay lines??
-### Or should I do biplots? 
-plot(Pval_12_inter,Pval_21_inter,xlim=c(0,1),ylim=c(0,1))
-plot(Pval_12_noInter,Pval_21_noInter,xlim=c(0,1),ylim=c(0,1))
-### Second hypothesis consistent with the null... 
+# Almost the same thing
+
+DataCompet_sugiharaDeterModel_CCM = data.frame(lag_order_inter,Pval_12_inter,Pval_21_inter,lag_order_noInter,Pval_12_noInter,Pval_21_noInter)
+
 
 ########################################################################################################################
 ########## Stochastic competition model -- interactions
