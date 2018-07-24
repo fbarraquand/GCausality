ca2e9dc66888bac0b92a230c3370fc3fa7fc56f2
frederic <frederic.barraquand@u-bordeaux.fr>
2018-07-24 09:48:46 +0200

    Added loop to compute CCM p-values for many repeats

--- twoSpecies_andDriver/2competitors_and_climate_withSurrogates.R
+++ twoSpecies_andDriver/2competitors_and_climate_withSurrogates.R
@@ -8,6 +8,7 @@
 # see https://cran.r-project.org/web/packages/rEDM/vignettes/rEDM-tutorial.html
 
 rm(list=ls())
+.libPaths("/home/frederic/myRpackages/")
 dev.off()
 
 ## Loading packages
@@ -218,8 +219,155 @@ for (i in 1:num_surr) {
 ### What if we used surrogate techniques in a time-domain Granger context? 
 ### As we did for Spectral GC!! Would that produce a fair comparison? 
 
-### NB Code with many simulations below is unchanged. Need to do that. 
-### ------------------------------------------------------------------###
+
+############################# Using many simulations to have distributions of P-values #############
+
+# Use of a function to test for CCM between time series x and y given temp
+ccm_surrogate_testing = function(y,y1,num_surr=1000){
+  
+  S12T=data.frame(1:tmax,y,y1) #species2_species1_temp
+  names(S12T)=c("time","species1","species2","temp")
+  ### Adapted from https://cran.r-project.org/web/packages/rEDM/vignettes/rEDM-tutorial.html
+  
+  ### Basic CCM analysis
+  varnames <- colnames(S12T[2:4])
+  n <- NROW(S12T)
+  ccm_matrix <- array(NA, dim = c(length(varnames), length(varnames)), dimnames = list(varnames, 
+                                                                                       varnames))
+  
+  for (ccm_from in varnames) {
+    for (ccm_to in varnames[varnames != ccm_from]) {
+      out_temp <- ccm(S12T, E = 8, lib_column = ccm_from, target_column = ccm_to, 
+                      lib_sizes = n, replace = FALSE, silent = TRUE)
+      ccm_matrix[ccm_from, ccm_to] <- out_temp$rho
+    }
+  }
+  
+  surr_temp_twin <- make_surrogate_data(S12T$temp, method = "twin", num_surr = num_surr)
+  surr_species2_twin <- make_surrogate_data(S12T$species2, method = "twin", num_surr = num_surr)
+  rho_surr1_twin <- data.frame(temp = numeric(num_surr), species2 = numeric(num_surr))
+  
+  for (i in 1:num_surr) {
+    rho_surr1_twin$temp[i] <- ccm(cbind(S12T$species1, surr_temp_twin[,i]), E = 3, lib_column = 1, target_column = 2, lib_sizes = NROW(S12T), 
+                                  replace = FALSE)$rho
+    
+    rho_surr1_twin$species2[i] <- ccm(cbind(S12T$species1, surr_species2_twin[,i]), E = 3, lib_column = 1, target_column = 2, lib_sizes = NROW(S12T), 
+                                      replace = FALSE)$rho
+  }
+  
+  ### P-value for rejecting the hypothesis that seasonality both causes temp and species 1
+  Pval_seasonCauses1 = (sum(ccm_matrix['species1','temp'] < rho_surr1_twin$temp) + 1) / 
+    (length(rho_surr1_twin$temp) + 1) # 0.87 
+  ### conclusion: cannot say that species1 xmap temp (equivalent to temp -> species 1)
+  
+  ### P-value for rejecting the hypothesis that seasonality both causes species 1 and species 2
+  # (more interesting here)
+  Pval_21= (sum(ccm_matrix['species1','species2'] < rho_surr1_twin$species2) + 1) / 
+    (length(rho_surr1_twin$species2) + 1) ## 0.24 
+  ### conclusion: cannot say that species 1 xmap species 2, i.e. species 2 causes species 1
+  
+  ### (NB we have to check the other way around, i.e., if there is an interaction the technique can detect it. 
+  ### cf. Cobey and Baskerville results.)
+  
+  ### Other causal direction -- we try to have species2 xmap species 1 to see if 1 causes 2
+  surr_species1_twin <- make_surrogate_data(S12T$species1, method = "twin", num_surr = num_surr)
+  rho_surr2_twin <- data.frame(temp = numeric(num_surr), species1 = numeric(num_surr))
+  
+  for (i in 1:num_surr) {
+    rho_surr2_twin$temp[i] <- ccm(cbind(S12T$species2, surr_temp_twin[,i]), E = 3, lib_column = 1, target_column = 2, lib_sizes = NROW(S12T), 
+                                  replace = FALSE)$rho
+    
+    rho_surr2_twin$species2[i] <- ccm(cbind(S12T$species2, surr_species1_twin[,i]), E = 3, lib_column = 1, target_column = 2, lib_sizes = NROW(S12T), 
+                                      replace = FALSE)$rho
+  }
+  
+  ### P-value for rejecting the hypothesis that temp does not cause species 2 fluctuations
+  Pval_seasonCauses2 = (sum(ccm_matrix['species2','temp'] < rho_surr2_twin$temp) + 1) / 
+    (length(rho_surr2_twin$temp) + 1) ### 0.000999001
+  ### we reject the hypothesis of joint fluctuation, species 2 xmap temp
+  
+  ### P-value for rejecting the hypothesis that species 1 does not cause species 2 fluctuations
+  Pval_12 = (sum(ccm_matrix['species2','species1'] < rho_surr2_twin$species2) + 1) / 
+    (length(rho_surr1_twin$species2) + 1) ## 0.07692308 almost significant here 
+  ### we find that species2 xmap species1 so that 1->2. 
+  
+  return(c(Pval_12,Pval_21))
+  
+}
+
+
+
+nsims=100
+p12=p12_noInter=rep(NA,nsims)#initializing
+p21=p21_noInter=rep(NA,nsims)
+
+### Loop over repeats
+for (krep in 1:nsims){
+  
+krep
+  
+tmax=300
+
+### Model with interactions
+Y=matrix(1,nrow=tmax,ncol=2)
+Y[1,1]=abs(rnorm(1,1,1))
+Y[1,2]=abs(rnorm(1,1,1))
+### Without interactions
+Z=matrix(1,nrow=tmax,ncol=2)
+Z[1,1]=abs(rnorm(1,1,1))
+Z[1,2]=abs(rnorm(1,1,1))
+
+seasonality<-2*sin(2*pi*(1:tmax)/24)	# must be enough to affect the growth rates
+### Environmental variables
+y1noise<-arima.sim(model=list(ar=c(0.1, 0.2, 0.1,0.5,-0.1)), n=tmax,sd=sqrt(0.5) )
+###y2noise<-arima.sim(model=list(ar=c(0.1, 0.2, 0.1,0.5,-0.1)), n=n.time,sd=sqrt(1) ) # more noisy
+y1<-seasonality+y1noise
+##y2<-seasonality+y2noise
+
+for (t in 1:(tmax-1)){
+  Y[t+1,1] = Y[t,1]*exp(3+0.5*y1[t] - 4*Y[t,1]-2*Y[t,2] + rnorm(1,0,0.1))
+  Y[t+1,2] = Y[t,2]*exp(2.1+0.5*y1[t] -0.31*Y[t,1]-3.1*Y[t,2] + rnorm(1,0,0.1))
+  Z[t+1,1] = Z[t,1]*exp(3+0.5*y1[t] - 4*Z[t,1]-0*Z[t,2] + rnorm(1,0,0.1))
+  Z[t+1,2] = Z[t,2]*exp(2.1+0.5*y1[t] -0*Z[t,1]-3.1*Z[t,2] + rnorm(1,0,0.1))
+}
+
+y=log(Y)
+z=log(Z)
+
+pccm = ccm_surrogate_testing(y,y1)
+p12[krep] = pccm[1]
+p21[krep] = pccm[2]
+
+
+pccm = ccm_surrogate_testing(z,y1)
+p12_noInter[krep]=pccm[1]
+p21_noInter[krep]=pccm[2]
+
+}
+
+#Fraction of 1->2 when present
+sum(p12<0.1)/nsims
+#Fraction of 2->1 when present
+sum(p21<0.1)/nsims
+
+#Fraction of 1->2 when absent
+sum(p12_noInter<0.1)/nsims
+#Fraction of 2->1 when absent
+sum(p21_noInter<0.1)/nsims
+
+library(sm)
+
+par(mfrow=c(2,2))
+## three groups, each of length: length(x), length(y), length(z)
+group.index <- rep(1:2, c(length(p12), length(p21)))
+## collect data together and use sm.density.compare()
+den <- sm.density.compare(c(p12,p21), group = group.index, model = "none") #"equal" does bootstrap for comparison
+
+group.index <- rep(1:2, c(length(p12_noInter), length(p21_noInter)))
+## collect data together and use sm.density.compare()
+den <- sm.density.compare(c(p12_noInter,p21_noInter), group = group.index, model = "none") #"equal" does bootstrap for comparison
+
+
 
 #### ------------- Old analysis 08/10/2017 ------------------------------------------####
 # pdf(file="CCM_2competitors_and_envDriver.pdf",height=10,width=10)
@@ -275,106 +423,6 @@ for (i in 1:num_surr) {
 
 ### We recover the correct causal order for effects of the environment
 ### Though it looks like species 1 affects species 2 with CCM
-
-############################# Using many simulations to have distributions of P-values #############
-nsims=100
-p12=p12_noExo=p12_noInter=p12_noInter_noExo=rep(1,nsims)#initializing
-p21=p21_noExo=p21_noInter=p21_noInter_noExo=rep(1,nsims)
-### Loop over repeats
-for (krep in 1:nsims){
-tmax=300
-
-### Model with interactions
-Y=matrix(1,nrow=tmax,ncol=2)
-Y[1,1]=abs(rnorm(1,1,1))
-Y[1,2]=abs(rnorm(1,1,1))
-### Without interactions
-Z=matrix(1,nrow=tmax,ncol=2)
-Z[1,1]=abs(rnorm(1,1,1))
-Z[1,2]=abs(rnorm(1,1,1))
-
-seasonality<-2*sin(2*pi*(1:tmax)/24)	# must be enough to affect the growth rates
-### Environmental variables
-y1noise<-arima.sim(model=list(ar=c(0.1, 0.2, 0.1,0.5,-0.1)), n=tmax,sd=sqrt(0.5) )
-###y2noise<-arima.sim(model=list(ar=c(0.1, 0.2, 0.1,0.5,-0.1)), n=n.time,sd=sqrt(1) ) # more noisy
-y1<-seasonality+y1noise
-##y2<-seasonality+y2noise
-
-for (t in 1:(tmax-1)){
-  Y[t+1,1] = Y[t,1]*exp(3+0.5*y1[t] - 4*Y[t,1]-2*Y[t,2] + rnorm(1,0,0.1))
-  Y[t+1,2] = Y[t,2]*exp(2.1+0.5*y1[t] -0.31*Y[t,1]-3.1*Y[t,2] + rnorm(1,0,0.1))
-  Z[t+1,1] = Z[t,1]*exp(3+0.5*y1[t] - 4*Z[t,1]-0*Z[t,2] + rnorm(1,0,0.1))
-  Z[t+1,2] = Z[t,2]*exp(2.1+0.5*y1[t] -0*Z[t,1]-3.1*Z[t,2] + rnorm(1,0,0.1))
-}
-
-y=log(Y)
-z=log(Z)
-
-varcompet2<-VAR(y, type="none",exogen=y1,lag.max=10,ic="SC") ### this is too high a lagmax
-c21=causality(varcompet2,cause="y1") #same notation as in the interaction matrix // effect of 1 on 2
-c12=causality(varcompet2,cause="y2")
-
-p12[krep]=c12$Granger$p.value
-p21[krep]=c21$Granger$p.value
-
-varcompet2_noExo=VAR(y, type="none",lag.max=10,ic="SC") ## Direct pairwise GC
-c21=causality(varcompet2_noExo,cause="y1")
-c12=causality(varcompet2_noExo,cause="y2") 
-
-p12_noExo[krep]=c12$Granger$p.value
-p21_noExo[krep]=c21$Granger$p.value
-
-varcompet2bis<-VAR(z, type="none",exogen=y1,lag.max=10,ic="SC")
-c21=causality(varcompet2bis,cause="y1") #same notation as in the interaction matrix // effect of 1 on 2
-c12=causality(varcompet2bis,cause="y2")
-
-p12_noInter[krep]=c12$Granger$p.value
-p21_noInter[krep]=c21$Granger$p.value
-
-varcompet2bis_noExo<-VAR(z, type="none",lag.max=10,ic="SC")
-c21=causality(varcompet2bis_noExo,cause="y1") #same notation as in the interaction matrix // effect of 1 on 2
-c12=causality(varcompet2bis_noExo,cause="y2")
-
-
-p12_noInter_noExo[krep]=c12$Granger$p.value
-p21_noInter_noExo[krep]=c21$Granger$p.value
-
-}
-
-library(sm)
-
-par(mfrow=c(2,2))
-## three groups, each of length: length(x), length(y), length(z)
-group.index <- rep(1:2, c(length(p12), length(p21)))
-## collect data together and use sm.density.compare()
-den <- sm.density.compare(c(p12,p21), group = group.index, model = "none") #"equal" does bootstrap for comparison
-
-group.index <- rep(1:2, c(length(p12_noExo), length(p21_noExo)))
-## collect data together and use sm.density.compare()
-den <- sm.density.compare(c(p12_noExo,p21_noExo), group = group.index, model = "none") #"equal" does bootstrap for comparison
-
-group.index <- rep(1:2, c(length(p12_noInter), length(p21_noInter)))
-## collect data together and use sm.density.compare()
-den <- sm.density.compare(c(p12_noInter,p21_noInter), group = group.index, model = "none") #"equal" does bootstrap for comparison
-
-group.index <- rep(1:2, c(length(p12_noInter_noExo), length(p21_noInter_noExo)))
-## collect data together and use sm.density.compare()
-den <- sm.density.compare(c(p12_noInter_noExo,p21_noInter_noExo), group = group.index, model = "none") #"equal" does bootstrap for comparison
-
-### Not very illustrative
-
-p12>0.05 ## Should be mostly false given there is causality 2->1
-p12_noInter>0.05 ## Should be mostly true since there is not causality 2->1
-### Many p-values stay quite low. For this F-test at least. 
-
-# other stuff to plot densities
-# ylim <- range(dx$y, dy$y, dz$y)
-# ## make plot
-# plot(dx$x, dx$y, col = 1, lwd = 2, type = "l", xlim = xlim, ylim = ylim)
-# lines(dy$x, dy$y, col = 2, lwd = 2)
-# lines(dz$x, dz$y, col = 3, lwd = 2)
-
-
 
 
 
