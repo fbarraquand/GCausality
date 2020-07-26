39d470d49d4d2633534474449effa73931e786fa
fbarraquand <frederic.barraquand@u-bordeaux.fr>
2020-07-26 19:06:36 +0200

    Added deterministic-counterpart model behaviour (two-cycle)

--- Lyapunov/StochasticCompet_LE.R
+++ Lyapunov/StochasticCompet_LE.R
@@ -15,11 +15,16 @@ sigma = 0.1 #Note: we have sigma=0.1 and not sigma^2 which is 0.01
 ### 1. Simulation of the model for a long time
 tmax = 10000
 x=matrix(0,nrow=2,ncol=tmax)
+xd=matrix(0,nrow=2,ncol=tmax) # deterministic sim. 
+
 
 x[,1] = c(0,0.1)
+xd[,1] = c(0,0.1)
+
 
 for (t in 1:(tmax-1)){
   x[,t+1] = x[,t] + r + a %*% exp(x[,t]) + rnorm(2,0,sigma)# Ricker compet model on the log-scale
+  xd[,t+1] = xd[,t] + r + a %*% exp(xd[,t])  # Ricker compet model on the log-scale
   #x[,t+1] = x[,t] + r + a %*% exp(x[,t]) + rnorm(2,0,sqrt(0.1))# More noise
 }
 
@@ -29,6 +34,14 @@ plot(time_interval,x[1,time_interval],type="b",col="blue")
 lines(time_interval,x[2,time_interval],type="b",col="red")
 #OK
 
+# Plotting deterministic version
+plot(time_interval,xd[1,time_interval],type="b",col="blue",ylim=c(-3,1))
+lines(time_interval,xd[2,time_interval],type="b",col="red")
+# Zoom
+time_interval = 780:800
+plot(time_interval,xd[1,time_interval],type="b",col="blue",ylim=c(-3,1))
+lines(time_interval,xd[2,time_interval],type="b",col="red")
+# The attractor is a two cycle -> with a negative deterministic LE
 
 ### 2. Definition of the Jacobian
 Jacobian <-function(x,a){
@@ -53,6 +66,7 @@ productStemp=S;
 for (t in 2:tmax){
   J = Jacobian(x[,t],a)
   s[t]=norm(J%*%productStemp,type="2");
+  #s[t]=norm(J%*%productStemp,type="f"); #other possibility. 
   S=J/s[t];
   productStemp=S%*%productStemp;
 }
