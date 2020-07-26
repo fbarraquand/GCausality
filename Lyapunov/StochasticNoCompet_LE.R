4b2725dfe4960a9fb08b5a941aac4f0ed25e24cd
fbarraquand <frederic.barraquand@u-bordeaux.fr>
2020-07-26 19:08:24 +0200

    Computed SLE and LE differently for species 1 and 2 since dynamically independent

--- Lyapunov/StochasticNoCompet_LE.R
+++ Lyapunov/StochasticNoCompet_LE.R
@@ -15,11 +15,15 @@ sigma = 0.1 #Note: we have sigma=0.1 and not sigma^2 which is 0.01
 ### 1. Simulation of the model for a long time
 tmax = 10000
 x=matrix(0,nrow=2,ncol=tmax)
+xd=matrix(0,nrow=2,ncol=tmax) # deterministic sim. 
 
 x[,1] = c(0,0.1)
+xd[,1] = c(0,0.1)
+
 
 for (t in 1:(tmax-1)){
   x[,t+1] = x[,t] + r + a %*% exp(x[,t]) + rnorm(2,0,sigma)# Ricker compet model on the log-scale
+  xd[,t+1] = xd[,t] + r + a %*% exp(xd[,t])  # Ricker compet model on the log-scale
   #x[,t+1] = x[,t] + r + a %*% exp(x[,t]) + rnorm(2,0,sqrt(0.1))# More noise
 }
 
@@ -29,6 +33,15 @@ plot(time_interval,x[1,time_interval],type="b",col="blue")
 lines(time_interval,x[2,time_interval],type="b",col="red")
 #OK
 
+# Plotting deterministic version
+plot(time_interval,xd[1,time_interval],type="b",col="blue")
+lines(time_interval,xd[2,time_interval],type="b",col="red")
+# Zoom
+time_interval = 780:800
+plot(time_interval,xd[1,time_interval],type="b",col="blue")
+lines(time_interval,xd[2,time_interval],type="b",col="red")
+# The det. attractor is a two cycle for species 2 -> with a negative deterministic LE
+# The det. attractor may be an invariant loop for species 1. 
 
 ### 2. Definition of the Jacobian
 Jacobian <-function(x,a){
@@ -40,29 +53,38 @@ Jacobian <-function(x,a){
   return(J)
 }
 
-
 ### 3. LE computation
+# for the two species independently, since these are independent. 
 
 #Initializing
-s=rep(1,tmax)
+s1=s2=rep(1,tmax)
+s1deter=s2deter=rep(1,tmax)
 J1 = Jacobian(x[,1],a)
-s[1]=norm(J1,type="2");
-S=J1/s[1];
-productStemp=S;
+J1d = Jacobian(xd[,1],a)
+
+s1[1]=abs(J1[1,1]);
+s2[1]=abs(J1[2,2]);
+s1deter[1]=abs(J1d[1,1]);
+s2deter[1]=abs(J1d[2,2]);
 
 for (t in 2:tmax){
   J = Jacobian(x[,t],a)
-  s[t]=norm(J%*%productStemp,type="2");
-  S=J/s[t];
-  productStemp=S%*%productStemp;
+  Jd = Jacobian(xd[,t],a)
+  s1[t] = abs(J[1,1]);
+  s2[t] = abs(J[2,2]);
+  s1deter[t] = abs(Jd[1,1]);
+  s2deter[t] = abs(Jd[2,2]);
 }
 
-LE=(1/tmax)*sum(log(s));
+LE1=(1/tmax)*sum(log(s1));
+LE2=(1/tmax)*sum(log(s2));
 
-LE
+LEd1=(1/tmax)*sum(log(s1deter));
+LEd2=(1/tmax)*sum(log(s2deter));
 
-# -0.1950553 (tmax = 10000)
-# -0.1896428 (tmax = 10000)
-# -0.1927537 (tmax = 10000)
+LE1 #positive
+LE2 #negative
 
-### Note: with more noise we go closer to noise-induced chaos (sensu Ellner and Turchin) without fully reaching it. 
+LEd1 #pos., beginning of chaos
+LEd2 #should be negative since this is a two-cycle
+
